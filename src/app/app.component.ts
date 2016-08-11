aa381ab0b9286fe295514e5f25ef3b0f7251418b
Bertus van Dalen <Bertus.vanDalen@ns.nl>
2016-08-11 21:43:39 +0200

    the app now compiles and uploads and loads imgs and sheets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>interactive content here</h1>'
 })
 export class AppComponent { }
