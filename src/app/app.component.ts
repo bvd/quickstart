5a36a24d68080f1218bba6484082ce71c7d080f5
Bertus van Dalen <Bertus.vanDalen@ns.nl>
2016-08-12 07:53:42 +0200

    angular heroes tutorial adding data to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,15 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>interactive content here</h1>'
+    template: '<h1>{{title}}</h1><h2>{{gm_title}} details</h2>'
 })
-export class AppComponent { }
+export class AppComponent { 
+	title = "Meldpunt Geluksmomenten";
+	// geluksmoment
+	gm_name = "Henk van Veen";
+	gm_email = "h.veen@gmail.com";
+	gm_title = "mijn rozenbotteljam";
+	gm_location = "keukenkast";
+	gm_description = "als ik mijn potten met rozenbotteljam zie";
+	gm_image = "f92ndosiw02mvlw9w92pdmsp2840suw0.jpg";
+}
