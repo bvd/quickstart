bd6a0a4c18f20c0ffacd1c4f77079d644cdf9aab
Bertus van Dalen <Bertus.vanDalen@ns.nl>
2016-08-12 08:14:00 +0200

    added all properties of happy moment to view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,24 @@ import { HappyMoment } from './happymoment';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{happymoment.title}} details</h2>'
+    template: `
+	  <h1>{{title}}</h1>
+	  <h2>{{happymoment.title}} details</h2>
+	  <div><label>id: </label>{{happymoment.id}}</div>
+	  <div><label>name: </label>{{happymoment.name}}</div>
+	  <div><label>email: </label>{{happymoment.email}}</div>
+	  <div><label>title: </label>{{happymoment.title}}</div>
+	  <div><label>location: </label>{{happymoment.location}}</div>
+	  <div><label>description: </label>{{happymoment.description}}</div>
+	  <div><label>image: </label>{{happymoment.image}}</div>
+	`
 })
 export class AppComponent { 
 	title = "Meldpunt Geluksmomenten";
 	
 	// geluksmoment
 	happymoment: HappyMoment = {
+		id: 1,
 		name: "Henk van Veen",
 	    email: "h.veen@gmail.com",
 	    title: "de rozenbotteljam",
