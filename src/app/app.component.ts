7297fb033b5e7a1edbcfbd448a7cffed78e26bbf
Bertus van Dalen <Bertus.vanDalen@ns.nl>
2016-08-12 08:07:12 +0200

    added class for happy moment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
-
+import { HappyMoment } from './happymoment';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{gm_title}} details</h2>'
+    template: '<h1>{{title}}</h1><h2>{{happymoment.title}} details</h2>'
 })
 export class AppComponent { 
 	title = "Meldpunt Geluksmomenten";
+	
 	// geluksmoment
-	gm_name = "Henk van Veen";
-	gm_email = "h.veen@gmail.com";
-	gm_title = "mijn rozenbotteljam";
-	gm_location = "keukenkast";
-	gm_description = "als ik mijn potten met rozenbotteljam zie";
-	gm_image = "f92ndosiw02mvlw9w92pdmsp2840suw0.jpg";
+	happymoment: HappyMoment = {
+		name: "Henk van Veen",
+	    email: "h.veen@gmail.com",
+	    title: "de rozenbotteljam",
+	    location: "keukenkast",
+	    description: "als ik mijn potten met rozenbotteljam zie",
+	    image: "f92ndosiw02mvlw9w92pdmsp2840suw0.jpg"
+	};
 }
---
+++ src/app/happymoment.ts
@@ -0,0 +1,9 @@
+export class HappyMoment {
+	id: number;
+	name: string;
+	email: string;
+	title: string;
+	location: string;
+	description: string;
+	image: string;
+}
